52a59e803dca01799680a2eb16d09836fa50252c
18016517 <18016517@myrp.edu.sg>
2020-06-16 15:14:45 +0800

    1

--- app/src/main/java/sg/edu/rp/c346/round3/HomeFragment.java
+++ app/src/main/java/sg/edu/rp/c346/round3/HomeFragment.java
@@ -4,23 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    EditText bmi, bloodPressure, bodyFat, quadPower, rackPull, agility;
+    Button submit;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        bmi = root.findViewById(R.id.editTextDP);
+        submit = root.findViewById(R.id.buttonSubmit);
+
         return root;
     }
 }
