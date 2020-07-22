eb7eb4df754d94ba58fe0c62cbcfe3a9854943bc
18016517 <18016517@myrp.edu.sg>
2020-07-22 15:02:22 +0800

    1

--- app/src/main/java/sg/edu/rp/c346/round3/EntryFragment/EntryFragment.java
+++ app/src/main/java/sg/edu/rp/c346/round3/EntryFragment/EntryFragment.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +16,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -33,6 +38,7 @@ public class EntryFragment extends Fragment {
     EditText weight, height, sp, dp, bodyFat, quadPower, rackPull, agility;
     Button submit;
     String a = "GtoqSX78uQUAwarX3wpR";
+    int count = 0;
 
     FirebaseFirestore db;
     CollectionReference dataRef;
@@ -56,6 +62,22 @@ public class EntryFragment extends Fragment {
         agility = root.findViewById(R.id.editTextAgility);
         submit = root.findViewById(R.id.buttonSubmit);
 
+        db.collection("/User/" + a + "/Data")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot querySnapshot = task.getResult();
+                            count = querySnapshot.size();
+                            Log.d("count", count + "");
+                        } else {
+                            Log.d("FireStore Error", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        Log.d("count", count + "");
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,17 +94,14 @@ public class EntryFragment extends Fragment {
                     double weightDouble = Double.parseDouble(weight.getText().toString());
                     double heightDouble = Double.parseDouble(height.getText().toString());
                     final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-                    final int i = prefs.getInt("increment", 1);
-                    DataEntry de = new DataEntry(spDouble, dpDouble, bodyFatDouble, quadPowerDouble, rackPullDouble, agilityDouble, weightDouble, heightDouble, c);
+                    count++;
 
-                   db.collection("/User/" + a + "/Data").document("Entry"+i).set(de).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    DataEntry de = new DataEntry(spDouble, dpDouble, bodyFatDouble, quadPowerDouble, rackPullDouble, agilityDouble, weightDouble, heightDouble, c);
+                    Log.d("count", count + "");
+                   db.collection("/User/" + a + "/Data").document("Entry"+count).set(de).addOnSuccessListener(new OnSuccessListener<Void>() {
                        @Override
                        public void onSuccess(Void aVoid) {
                            Toast.makeText(getContext(), "Entry Added Successfully", Toast.LENGTH_LONG).show();
-                           int newValue = i + 1;
-                           SharedPreferences.Editor prefEdit = prefs.edit();
-                           prefEdit.putInt("increment", newValue);
-                           prefEdit.commit();
                        }
                    }).addOnFailureListener(new OnFailureListener() {
                        @Override
